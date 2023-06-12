5b68302176d03fdf311646a35bfd740611621b5e
Guilherme Costa Sales <guilhermecosales@gmail.com>
2023-06-12 01:32:59 -0300

    feat: entity repository

---
+++ src/main/java/com/github/peoplemanagement/repository/AddressRepository.java
@@ -0,0 +1,11 @@
+package com.github.peoplemanagement.repository;
+
+import com.github.peoplemanagement.entity.Address;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface AddressRepository extends JpaRepository<Address, UUID> {
+}
---
+++ src/main/java/com/github/peoplemanagement/repository/PersonRepository.java
@@ -0,0 +1,11 @@
+package com.github.peoplemanagement.repository;
+
+import com.github.peoplemanagement.entity.Person;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface PersonRepository extends JpaRepository<Person, UUID> {
+}
