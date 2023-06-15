1c988228d84e7604b029be908f9816fcc82941d6
Guilherme Costa Sales <guilhermecosales@gmail.com>
2023-06-15 09:40:54 -0300

    feat: add JSON Include

--- src/main/java/com/github/peoplemanagement/dto/response/PersonDto.java
+++ src/main/java/com/github/peoplemanagement/dto/response/PersonDto.java
@@ -1,5 +1,6 @@
 package com.github.peoplemanagement.dto.response;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.github.peoplemanagement.entity.Person;
 import lombok.Data;
 
@@ -13,6 +14,7 @@ import java.util.UUID;
  */
 
 @Data
+@JsonInclude(value = JsonInclude.Include.NON_NULL)
 public class PersonDto implements Serializable {
 
     @Serial
