ecde4d41fe220b3b10f650123ec774cda6b01716
Guilherme Costa Sales <guilhermecosales@gmail.com>
2023-06-24 17:12:32 -0300

    feat: override method to display fields with errors

--- src/main/java/com/github/peoplemanagement/controller/exception/RestExceptionHandler.java
+++ src/main/java/com/github/peoplemanagement/controller/exception/RestExceptionHandler.java
@@ -1,15 +1,30 @@
 package com.github.peoplemanagement.controller.exception;
 
 import com.github.peoplemanagement.service.exception.NotFoundException;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ProblemDetail;
-import org.springframework.http.ResponseEntity;
+import jakarta.validation.constraints.NotNull;
+import org.springframework.http.*;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.util.HashSet;
+import java.util.Set;
 
 @RestControllerAdvice
 public class RestExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, @NotNull HttpHeaders headers, @NotNull HttpStatusCode status, @NotNull WebRequest request) {
+        final Set<String> errors = new HashSet<>();
+        ex.getBindingResult().getFieldErrors().forEach(fieldError -> errors.add(fieldError.getField() + " " + fieldError.getDefaultMessage()));
+
+        final ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(status, "Invalid request content.");
+        problemDetail.setProperty("errors", errors);
+
+        return createResponseEntity(problemDetail, headers, status, request);
+    }
 
     @ExceptionHandler(value = NotFoundException.class)
     protected ResponseEntity<ProblemDetail> handleNotFoundException(NotFoundException exception) {
